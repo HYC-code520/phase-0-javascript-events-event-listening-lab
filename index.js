11d214f7b66d6619b2d3cc9a2988b38587c0f0c5
Ariel Chen <happilyeverafter0800@gmail.com>
2024-11-13 23:08:54 -0500

    complete lab

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
 function addingEventListener() {
-}
+    // Select the button element by its ID
+    const button = document.getElementById('button');
+  
+    // Add an event listener for the 'click' event
+    button.addEventListener('click', function () {
+      alert('Button was clicked!');
+    });
+  }
+  
